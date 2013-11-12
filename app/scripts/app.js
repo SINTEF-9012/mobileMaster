82c542703628e14fd3323cd7f09c92b713930c63
U-SINTEFGRP\antoinep <antoinep@SINTEFPC6066.sintef.no>
2013-11-12 09:56:40 +0100

    First version

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('mobileMasterApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
