1a9f5940aa897a134e6f100b4827f70a73cc242b
Antoine Pultier <antoine.pultier@sintef.no>
2013-12-16 15:42:40 +0100

    Refresh the scroll view when the scope is updated

--- app/scripts/controllers/main.ts
+++ app/scripts/controllers/main.ts
@@ -1,7 +1,17 @@
-'use strict';
+/// <reference path="./../references/angularjs/angular.d.ts" />
 
+declare var IScroll;
+
+'use strict';
 angular.module('mobileMasterApp')
-  .controller('MainCtrl', function ($scope, nodeMaster) {
+  .controller('MainCtrl', function ($scope, nodeMaster, $timeout : ng.ITimeoutService) {
 
   	
+	var myScroll = new IScroll('#wrapper', { mouseWheel: true, scrollX: true, scrollY: true, zoom:true});
+
+	$scope.$watch('[patients, resources]', function() {
+		$timeout(function() {
+			myScroll.refresh();
+		}); 	
+	});
   });
