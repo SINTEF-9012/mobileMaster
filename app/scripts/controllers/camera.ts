4c47d1e5e0d53aa3b9319fa9b912180dfab0b680
Antoine Pultier <antoine.pultier@sintef.no>
2014-04-24 17:01:29 +0200

    Now the camera can record movies, and even create PDF !

--- app/scripts/controllers/camera.ts
+++ app/scripts/controllers/camera.ts
@@ -2,12 +2,29 @@
 
 angular.module('mobileMasterApp')
 	.config((AddServiceProvider: AddServiceConfig) => {
-		AddServiceProvider.defineType(
+
+		var pictureType =
 			ThingModel.BuildANewThingType.Named('master:picture')
 				.WhichIs('Picture')
 				.ContainingA.Location('location')
 				.AndA.NotRequired.String('url')
-				.AndA.NotRequired.String('description')
+				.AndA.NotRequired.String('description').Build();
+//				.AndA.NotRequired.DateTime('creation')
+//				.WhichIs('Creation date')
+
+
+		AddServiceProvider.defineType(pictureType);
+
+		AddServiceProvider.defineType(
+			ThingModel.BuildANewThingType.Named('master:video')
+				.WhichIs('Video')
+				.ContainingA.CopyOf(pictureType)
+				.Build());
+
+		AddServiceProvider.defineType(
+			ThingModel.BuildANewThingType.Named('master:document')
+				.WhichIs('Document')
+				.ContainingA.CopyOf(pictureType)
 				.Build());
 	})
 	.controller('CameraCtrl', (
@@ -21,23 +38,30 @@ angular.module('mobileMasterApp')
 
 			var p = $stateParams.hash + '.' + $stateParams.extension,
 				s = settingsService.getMediaServerUrl();
-		$scope.url = s + '/thumbnail/' + p;
+		$scope.url = s + '/' + p;
 //		$scope.identicon = settingsService.getMediaServerUrl() + '/identicon/' + $stateParams.hash;
 
-		var camThumb = $('#camera-thumbnail');
 
-		camThumb.on('error', () => {
-			$('#camera-submit').removeClass('btn-primary').addClass('btn-danger').text('Publish anyway');
-			$('<div class="alert alert-danger">The picture seems to be broken.</div>').insertAfter(camThumb);
-			camThumb.remove();
-		});
+		// Small list but it should be enough in 2014 (and we support web)
+		$scope.isPicture = /(bmp|png|jpeg|jpg|gif|tiff|webp)/i.test($stateParams.extension);
+		$scope.isVideo = /(3gb|3g2|h261|h263|h264|jpgv|mp4|mpv4|mpg4|mpeg|mpg|mpe|mv1|mv2|ogv|qt|mov|webm|flv|mkv|mk3d|wm|wmv|avi|movie)/i.test($stateParams.extension);
+		var type = $scope.isPicture ? 'master:picture' : ($scope.isVideo ? 'master:video' : 'master:document');
 
+		if ($scope.isPicture) {
+			$scope.thumbnailUrl = s + '/thumbnail/' + p;
+			var camThumb = $('#camera-thumbnail');
 
+			camThumb.on('error', () => {
+				$('#camera-submit').removeClass('btn-primary').addClass('btn-danger').val('Publish anyway');
+				$('<div class="alert alert-danger">The picture seems to be broken.</div>').insertAfter(camThumb);
+//				camThumb.remove();
+			});
+		}
 		var position = masterMap.getCenter();
 
 		var marker = new L.Marker(position, {
 			draggable: true,
-			icon: masterMap.createMasterIconWithType('picture',$scope)
+			icon: masterMap.createMasterIconWithType(type,$scope)
 		});
 
 		marker.addTo(masterMap);
@@ -71,9 +95,13 @@ angular.module('mobileMasterApp')
 		});
 
 		$scope.publish = () => {
-			AddService.register('master:picture', marker.getLatLng(), (thing : ThingModel.ThingPropertyBuilder) => {
-				thing.String('description', $scope.description);
-				thing.String('url', s + '/' + p);
+
+
+
+			AddService.register(type, marker.getLatLng(), (thing : ThingModel.ThingPropertyBuilder) => {
+				thing.String('description', $scope.description != undefined ? $scope.description : "");
+				thing.String('url', $scope.url);
+//				thing.DateTime('creation', new Date());
 			});
 			$state.go("main");
 		};
