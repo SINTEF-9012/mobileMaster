57d902d4d7e9ae87065c866cc3f84a987722ea40
Antoine Pultier <antoine.pultier@sintef.no>
2014-08-20 13:21:24 +0200

    humanizeCamelCase filter

---
+++ app/scripts/filters/humanizeCamelCase.ts
@@ -0,0 +1,10 @@
+/// <reference path="./../../bower_components/DefinitelyTyped/angularjs/angular.d.ts" />
+'use strict';
+
+angular.module('mobileMasterApp')
+	.filter('humanizeCamelCase', () => {
+	
+		// Thanks to angularjs-camelCase-to-human-filter
+		return (input: any): string =>
+			input.charAt(0).toUpperCase() + input.substr(1).replace(/[A-Z]/g, ' $&');
+	});
