7a978d341f42d68ef3fdc739e0c4fef4131d0f3a
Antoine Pultier <antoine.pultier@sintef.no>
2014-12-09 16:46:06 +0100

    Fancy charts on the dashboard

--- app/scripts/controllers/main.ts
+++ app/scripts/controllers/main.ts
@@ -199,6 +199,48 @@ angular.module('mobileMasterApp')
 
 	var destroyed = false;
 
+	// Chart.js Options
+	var chartJSOptions = {
+
+		// Sets the chart to be responsive
+		responsive: false,
+
+		//Boolean - Whether we should show a stroke on each segment
+		segmentShowStroke: false,
+
+		//String - The colour of each segment stroke
+		segmentStrokeColor: 'black',
+
+		//Number - The width of each segment stroke
+		segmentStrokeWidth: 2,
+
+		//Number - The percentage of the chart that we cut out of the middle
+		percentageInnerCutout: 60, // This is 0 for Pie charts
+
+		// Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
+		maintainAspectRatio: false,
+
+		//Number - Amount of animation steps
+		animationSteps: 100,
+
+		//String - Animation easing effect
+		animationEasing: 'easeOutBounce',
+
+		//Boolean - Whether we animate the rotation of the Doughnut
+		animateRotate: true,
+
+		//Boolean - Whether we animate scaling the Doughnut from the centre
+		animateScale: false,
+	};
+
+	// And for a doughnut chart
+	var patientsChart = new Chart((<HTMLCanvasElement>document.getElementById('patients-chart')).getContext('2d')).Doughnut([], chartJSOptions);
+	var resourcesChart = new Chart((<HTMLCanvasElement>document.getElementById('resources-chart')).getContext('2d')).Doughnut([], chartJSOptions);
+	var beaconsChart = new Chart((<HTMLCanvasElement>document.getElementById('beacons-chart')).getContext('2d')).Doughnut([], chartJSOptions);
+	patientsChart.resize();
+	resourcesChart.resize();
+	beaconsChart.resize();
+
 	var setLayout = throttle(() => {
 
 		if (destroyed) {
@@ -245,17 +287,31 @@ angular.module('mobileMasterApp')
 			jChatScrollarea.scrollTop = jChatScrollarea.scrollHeight;
 
 			masterMap.moveTo(jMapBody);
+			patientsChart.resize(() => {
+				patientsChart.reflow();
+				patientsChart.update();
+			});
+			resourcesChart.resize(() => {
+				resourcesChart.reflow();
+				resourcesChart.update();
+			});
+			beaconsChart.resize(() => {
+				beaconsChart.reflow();
+				beaconsChart.update();
+			});
 			//masterMap.invalidateSize({});
 			masterMap.showOverview();
+
 		});
 	}, 200);
 
 
-	var statsPatients: { [color: string]: number }, nbPatients = 0;
-	var patientsChart = new SuperSimpleCharts.BarChart(document.getElementById('patients-chart'));
+	//var statsPatients: { [color: string]: number }, nbPatients = 0;
+	//var patientsChart = new SuperSimpleCharts.BarChart(document.getElementById('patients-chart'));
+
 
 	var checkObserver = (thing: ThingModel.Thing) => {
-		if (itsa.patient(thing)) {
+		if (itsa.patient(thing) || itsa.beacon(thing) || itsa.resource(thing)) {
 			computeStats();
 		}
 
@@ -281,7 +337,7 @@ angular.module('mobileMasterApp')
 		}
 
 		var now = +new Date();
-		if (now - lastCall < interval) {
+		if ((now - lastCall) < interval) {
 			workerTimeout = window.setTimeout(computeStatsWorker, interval);
 			return;
 		}
@@ -289,34 +345,147 @@ angular.module('mobileMasterApp')
 		computeStatsWorker(first);
 	};
 
+	var triageColorsLockup = {
+		'unknown': '#666',
+		'no status entered': '#666',
+		'yellow': '#EBC813',
+		'red': '#E51E23' 
+	};
+
+
+	var statsResources: { value: number; color: string; highlight: string; label: string }[] = [
+		{
+			value: 1,
+			color: '#F7464A',
+			highlight: '#FF2539',
+			label: 'Fire'
+		},
+		{
+			value: 1,
+			color: '#318C12',
+			highlight: '#309D01',
+			label: 'Medic'
+		},
+		{
+			value: 1,
+			color: '#55BCBE',
+			highlight: '#44BCCF',
+			label: 'Police'
+		}
+	];
+	var statsBeacons: { value: number; color: string; highlight: string; label: string }[] = [
+		{
+			value: 1,
+			color: '#D2204C',
+			highlight: '#E56081',
+			label: 'Unsafe'
+		},
+		{
+			value: 1,
+			color: '#01579B',
+			highlight: '#327DB8',
+			label: 'Safe'
+		},
+		{
+			value: 1,
+			color: '#0A8F08',
+			highlight: '#50C04E',
+			label: 'Rescued'
+		}
+	];
+
 	var computeStatsWorker = (first?: boolean) => {
-		nbPatients = 0;
-		statsPatients = {};
+		workerTimeout = 0;
+		lastCall = +new Date();
+		//nbPatients = 0;
+		var statsPatients = {};
+		var finalStatsPatients = [];
+
+		patientsChart.segments = [];
+		resourcesChart.segments = [];
+		beaconsChart.segments = [];
+
+		statsResources[0].value = 0;
+		statsResources[1].value = 0;
+		statsResources[2].value = 0;
+
+		statsBeacons[0].value = 0;
+		statsBeacons[1].value = 0;
+		statsBeacons[2].value = 0;
+
 		angular.forEach(thingModel.warehouse.Things, (thing: ThingModel.Thing) => {
 			if (itsa.patient(thing)) {
-				nbPatients += 1;
+				//nbPatients += 1;
 
 				var triage_status: string;
 				if (!(triage_status = thing.GetString('triage_status'))) {
-					triage_status = '#FF4B00';
+					triage_status = 'unknown';
 				} else {
 					triage_status = triage_status.toLowerCase();
-					if (triage_status === 'yellow') {
-						triage_status = '#EBC813';
-					}
 				}
 
-				statsPatients[triage_status] = (statsPatients[triage_status] + 1) || 1;
+				if (statsPatients[triage_status]) {
+					++statsPatients[triage_status].value;
+				} else {
+					var s = statsPatients[triage_status] = {
+						value: 1,
+						color: triageColorsLockup[triage_status] || triage_status,
+						label: triage_status
+					};
+					finalStatsPatients.push(s);
+				}
+			} else if (itsa.resource(thing)){ 
+				if (itsa.fire(thing)) {
+					++statsResources[0].value;
+				} else if (itsa.medic(thing)) {
+					++statsResources[1].value;
+				} else if (itsa.police(thing)) {
+					++statsResources[2].value;
+				}
+			} else if (itsa.beacon(thing)) {
+				if (thing.Boolean("rescued")) {
+					++statsBeacons[2].value;
+				} else if (thing.Boolean("safe")) {
+					++statsBeacons[1].value;
+				} else {
+					++statsBeacons[0].value;
+				}
 			}
 		});
 
-		patientsChart.SetData(statsPatients);
-		$scope.nbPatients = nbPatients;
+		//patientsChart.SetData(statsPatients);
+		//$scope.nbPatients = nbPatients;
+		//myDoughnutChart.segments = $scope.statsPatients;
+		for (var i = 0, l = finalStatsPatients.length; i < l; ++i) {
+			patientsChart.addData(finalStatsPatients[i], undefined, true);
+		}
+		for (i = 0, l = statsResources.length; i < l; ++i) {
+			resourcesChart.addData(statsResources[i], undefined, true);
+		}
+		for (i = 0, l = statsBeacons.length; i < l; ++i) {
+			beaconsChart.addData(statsBeacons[i], undefined, true);
+		}
+		resourcesChart.stop();
+		resourcesChart.reflow();
+		resourcesChart.update();
+
+		patientsChart.stop();
+		patientsChart.reflow();
+		patientsChart.update();
+
+		beaconsChart.stop();
+		beaconsChart.reflow();
+		beaconsChart.update();
+
 		if (!first) {
 			$scope.$apply();
 			setLayout();
+			patientsChart.options.animateRotate = false;
+			resourcesChart.options.animateRotate = false;
+			beaconsChart.options.animateRotate = false;
 		}
 	};
+
 
 
 	var computeSummary = () => {
@@ -350,6 +519,9 @@ angular.module('mobileMasterApp')
 		destroyed = true;
 		jwindow.off('resize', setLayout);
 		thingModel.warehouse.UnregisterObserver(observer);
+		patientsChart.destroy();
+		resourcesChart.destroy();
+		beaconsChart.destroy();
 	});
 
 	jwindow.resize(setLayout);
